b311535ad95663f7cd6274df2a6657e13ec000b8
Ivan Kornienko <i@stackrox.com>
2020-04-24 00:42:15 -0700

    Make test pass

--- ui/src/App.test.tsx
+++ ui/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app with the proper header', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = getByText(/Setup Next UI/i);
+  expect(headerElement).toBeInTheDocument();
 });
