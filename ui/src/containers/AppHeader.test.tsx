957acbf0be6d5d730dd7513069e55df0f78598a5
Ivan Kornienko <i@stackrox.com>
2020-05-02 14:07:54 -0700

    Adjust tests

--- ui/src/containers/AppHeader.test.tsx
+++ ui/src/containers/AppHeader.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import AppHeader from './AppHeader';
 
 test('renders app with the proper header', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<AppHeader />);
   const headerElement = getByText(/Infra/i);
   expect(headerElement).toBeInTheDocument();
 });
