da8e825ca1bd414357ea34c94087841714cdf347
Ivan Kornienko <i@stackrox.com>
2020-05-05 09:03:48 -0700

    UI: add "Launch Cluster" section to the Home page (#53)

---
+++ ui/src/components/LinkCard.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement, ReactNode } from 'react';
+import { Link } from 'react-router-dom';
+
+type Props = {
+  to: string;
+  header: string;
+  children: ReactNode;
+  footer?: string;
+  className?: string;
+};
+
+export default function LinkCard({
+  to,
+  header,
+  children,
+  footer,
+  className = '',
+}: Props): ReactElement {
+  return (
+    <Link
+      className={`flex flex-col p-2 border-2 border-base-400 shadow rounded font-600 bg-base-100 hover:bg-base-200 text-base-600 hover:text-base-700 ${className}`}
+      to={to}
+    >
+      <h2 className="pb-1 text-2xl border-b-2 border-base-500 mb-4">{header}</h2>
+      {children}
+      {footer && <span className="mt-auto capitalize">{footer}</span>}
+    </Link>
+  );
+}
---
+++ ui/src/containers/HomePage/LaunchPageSection.tsx
@@ -0,0 +1,49 @@
+import React, { ReactElement } from 'react';
+import { AxiosPromise } from 'axios';
+
+import { V1FlavorListResponse, FlavorServiceApi } from 'generated/client';
+import useApiQuery from 'client/useApiQuery';
+import configuration from 'client/configuration';
+import PageSection from 'components/PageSection';
+import LinkCard from 'components/LinkCard';
+import FullPageSpinner from 'components/FullPageSpinner';
+import FullPageError from 'components/FullPageError';
+
+const flavorService = new FlavorServiceApi(configuration);
+
+const fetchFlavors = (): AxiosPromise<V1FlavorListResponse> => flavorService.list();
+
+function FlavorCards(): ReactElement {
+  const { loading, error, data } = useApiQuery(fetchFlavors);
+
+  if (loading) {
+    return <FullPageSpinner />;
+  }
+
+  if (error || !data?.Flavors) {
+    return <FullPageError message={error?.message || 'Unexpected server response'} />;
+  }
+
+  const cards = data.Flavors.map((flavor) => (
+    <LinkCard
+      key={flavor.ID}
+      to={`launch/${flavor.ID}`}
+      header={flavor.Name || 'Unnamed'}
+      footer={flavor.Availability}
+      className="s-1"
+    >
+      <p className="text-lg">{flavor.Description}</p>
+    </LinkCard>
+  ));
+  return <>{cards}</>;
+}
+
+export default function LaunchPageSection(): ReactElement {
+  return (
+    <PageSection header="Launch Cluster">
+      <div className="grid grid-gap-4 grid-auto-fit grid-dense">
+        <FlavorCards />
+      </div>
+    </PageSection>
+  );
+}
