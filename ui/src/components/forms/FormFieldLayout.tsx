64fcd991826e6f14e68a725f38502f0f7d15e42e
Ivan Kornienko <i@stackrox.com>
2020-05-06 14:36:15 -0700

    Oops, one file was left unsaved

--- ui/src/components/forms/FormFieldLayout.tsx
+++ ui/src/components/forms/FormFieldLayout.tsx
@@ -9,9 +9,9 @@ type Props = {
 export default function FormFieldLayout({ label, input, error }: Props): ReactElement {
   return (
     <div className="flex flex-col mb-4">
-      <div className="mb-1 text-base-600 font-700">{label}</div>
-      <div className="w-full my-1">{input}</div>
-      <div className="w-full my-1">{error}</div>
+      <div className="w-full mb-2">{label}</div>
+      <div className="w-full">{input}</div>
+      <div className="w-full mt-2">{error}</div>
     </div>
   );
 }
