95e18b186a921f9fd7f084b866b57092ccb2a30f
Van Wilson <vwilson@redhat.com>
2021-09-16 16:59:20 -0400

    RS-169: Add cluster type to instance tiles (#543)

--- ui/src/components/PageSection.tsx
+++ ui/src/components/PageSection.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function PageSection({ header, className = '', children }: Props): ReactElement {
   return (
-    <div className="flex flex-col h-full min-h-0">
+    <div className="border-t-3 border-base-400 flex flex-col h-full min-h-0">
       <div className={`h-full overflow-auto ${className}`}>
         <header className="border-b-2 border-base-400 px-4 py-2">
           <h2 className="bg-base-0 capitalize font-600 sticky text-4xl text-base-600 top-0">
