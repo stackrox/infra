dc2fec1a4013f8205550b06e8523ea74e8d80fc8
Gavin Jefferies <gavin@stackrox.com>
2022-10-05 02:36:44 +0000

    finesse

--- scripts/add_PR_comment_for_deploy_to_dev.sh
+++ scripts/add_PR_comment_for_deploy_to_dev.sh
@@ -18,32 +18,31 @@ add_PR_comment_for_deploy_to_dev() {
     cat > "$tmpfile" <<- EOT
 A single node development cluster ({{.Env.DEV_CLUSTER_NAME}}) was allocated in production infra for this PR.
 
-You can connect to this cluster with:
+You can connect to this cluster with: :electric_plug:
 \`\`\`
 gcloud container clusters get-credentials {{.Env.DEV_CLUSTER_NAME}} --zone us-central1-a --project srox-temp-dev-test
 \`\`\`
 
-And then deploy your development changes with:
+And then deploy your development infra-server with: :rocket:
 \`\`\`
 make render-local
 make install-local
 \`\`\`
 
-And pull infractl from the deployed dev infra-server with:
+And pull infractl from the deployed dev infra-server with: :hammer_and_wrench:
 \`\`\`
 nohup kubectl -n infra port-forward svc/infra-server-service 8443:8443 &
 make pull-infractl-from-dev-server
 \`\`\`
 
-You can then use the dev infra instance e.g.:
+You can then use the dev infra instance e.g.: :bike:
 \`\`\`
 bin/infractl -k -e localhost:8443 whoami
 \`\`\`
 
-:warning: ***Any clusters that you start in your dev infra instance should have a lifespan shorter 
-then the development cluster instance. Otherwise they will not be destroyed when the dev infra is 
-destroyed along with the development cluster.*** :warning:
-
+:warning: ***Any clusters that you start using your dev infra instance should have a lifespan shorter 
+then the development cluster instance. Otherwise they will not be destroyed when the dev infra instance 
+ceases to exist along when the development cluster is deleted.*** :warning:
 EOT
 
     hub-comment -type deploy -template-file "$tmpfile"
