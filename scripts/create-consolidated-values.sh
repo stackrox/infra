0ac2732dc3636be0ea02f9b740eb7234421e3478
Tom Martensen <tmartens@redhat.com>
2022-10-13 08:18:22 +0200

    trim newline; replaces os specific base64 command

--- scripts/create-consolidated-values.sh
+++ scripts/create-consolidated-values.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-case "$OSTYPE" in
-  darwin*)  export b64command="gbase64";;
-  linux*)   export b64command="base64" ;;
-  *)        echo "unknown OS: $OSTYPE"; exit 1 ;;
-esac
-
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
 source "$ROOT/scripts/lib.sh"
 
@@ -45,7 +39,7 @@ create_consolidated_values() {
         local helm_safe_key="${cfg_file//[.-]/_}"
         helm_safe_key="${helm_safe_key////__}"
 
-        echo "$helm_safe_key: $($b64command -w0 < "$cfg_file")" >> "$values_file"
+        echo "$helm_safe_key: $(base64 < "$cfg_file" | tr -d '\n')" >> "$values_file"
         echo >> "$values_file"
     done
     popd > /dev/null
