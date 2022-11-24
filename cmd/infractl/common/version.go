f97d9e848e6bc2e490947dda6555a60bc2d488a0
Tom Martensen <tmartens@redhat.com>
2022-11-24 15:56:47 +0100

    only inform cli user about version mismatch when minor or major versions mismatch

--- cmd/infractl/common/version.go
+++ cmd/infractl/common/version.go
@@ -2,6 +2,8 @@ package common
 
 import (
 	"context"
+	"regexp"
+	"strings"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/spf13/cobra"
@@ -10,15 +12,34 @@ import (
 	"google.golang.org/grpc"
 )
 
+func getMajorMinorVersion(cmd *cobra.Command, version *v1.Version) string {
+	versionStr := version.GetVersion()
+	if regexp.MustCompile(`\d+\.\d+\.\d+`).MatchString(versionStr) {
+		return strings.Join(strings.Split(versionStr, ".")[:2], ".")
+	}
+	return ""
+}
+
+func versionsDoNotMatch(cmd *cobra.Command, clientVersion *v1.Version, serverVersion *v1.Version) bool {
+	return getMajorMinorVersion(cmd, clientVersion) != getMajorMinorVersion(cmd, serverVersion)
+}
+
+func areLocalVersions(clientVersion *v1.Version, serverVersion *v1.Version) bool {
+	localVersionId := "local"
+	return clientVersion.GetVersion() == localVersionId || serverVersion.GetVersion() == localVersionId
+}
+
 func checkForVersionDiff(ctx context.Context, conn *grpc.ClientConn, cmd *cobra.Command) {
 	if cmd.Use == "version" || cmd.Use == "cli" {
 		return
 	}
 
 	clientVersion := buildinfo.All()
-	serverVersion, _ := v1.NewVersionServiceClient(conn).GetVersion(ctx, &empty.Empty{})
-
-	if serverVersion != nil && clientVersion.Version != serverVersion.Version {
+	serverVersion, err := v1.NewVersionServiceClient(conn).GetVersion(ctx, &empty.Empty{})
+	if err != nil {
+		cmd.PrintErrln("---\ncannot retrieve infra versions to compare.\n---")
+	}
+	if !areLocalVersions(clientVersion, serverVersion) && versionsDoNotMatch(cmd, clientVersion, serverVersion) {
 		cmd.PrintErrf("---\ninfractl and infra-server versions are different.\n"+
 			"%s -v- %s\n"+
 			"You can use `infractl cli upgrade` to update.\n---\n",
