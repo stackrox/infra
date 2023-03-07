8fb404684f9ec32825e3daf92d18f2fed93553b2
Gavin Jefferies <gavin@stackrox.com>
2023-03-06 17:51:11 -0800

    type

--- slack/client.go
+++ slack/client.go
@@ -2,10 +2,8 @@
 package slack
 
 import (
-	"context"
 	"log"
 	"sync"
-	"time"
 
 	"github.com/slack-go/slack"
 	"github.com/stackrox/infra/config"
@@ -14,8 +12,8 @@ import (
 // Slacker represents a type that can interact with the Slack API.
 type Slacker interface {
 	PostMessage(options ...slack.MsgOption) error
-	PostMessageToUser(user slack.User, options ...slack.MsgOption) error
-	LookupUser(email string) (slack.User, bool)
+	PostMessageToUser(user *slack.User, options ...slack.MsgOption) error
+	LookupUser(email string) (*slack.User, bool)
 }
 
 var _ Slacker = (*slackClient)(nil)
@@ -24,7 +22,7 @@ var _ Slacker = (*disabledSlack)(nil)
 type slackClient struct {
 	client     *slack.Client
 	channelID  string
-	emailCache map[string]slack.User
+	emailCache map[string]*slack.User
 	lock       sync.RWMutex
 }
 
@@ -34,11 +32,11 @@ func (s disabledSlack) PostMessage(options ...slack.MsgOption) error {
 	return nil
 }
 
-func (s disabledSlack) PostMessageToUser(user slack.User, options ...slack.MsgOption) error {
+func (s disabledSlack) PostMessageToUser(user *slack.User, options ...slack.MsgOption) error {
 	return nil
 }
-func (s disabledSlack) LookupUser(email string) (slack.User, bool) {
-	return slack.User{}, false
+func (s disabledSlack) LookupUser(email string) (*slack.User, bool) {
+	return &slack.User{}, false
 }
 
 // New creates a new Slack client that uses the given token for
@@ -54,7 +52,7 @@ func New(cfg *config.SlackConfig) (Slacker, error) {
 	client := &slackClient{
 		client:     slack.New(cfg.Token),
 		channelID:  cfg.Channel,
-		emailCache: make(map[string]slack.User),
+		emailCache: make(map[string]*slack.User),
 	}
 
 	log.Printf("[INFO] Enabled Slack integration")
@@ -62,7 +60,7 @@ func New(cfg *config.SlackConfig) (Slacker, error) {
 	return client, nil
 }
 
-func (s *slackClient) LookupUser(email string) (slack.User, bool) {
+func (s *slackClient) LookupUser(email string) (*slack.User, bool) {
 	s.lock.RLock()
 	user, found := s.emailCache[email]
 	if found {
@@ -71,13 +69,13 @@ func (s *slackClient) LookupUser(email string) (slack.User, bool) {
 	}
 	s.lock.RUnlock()
 
-	log.Printf("[DEBUG] Lookup user: %s", email)
-	users, err := s.client.GetUserByEmail(email)
+	log.Printf("[DEBUG] Lookup user by email: %s", email)
+	user, err := s.client.GetUserByEmail(email)
 	if err != nil {
 		log.Printf("[WARN] Lookup user error: %s, %v", email, err)
 		return nil, false
 	}
-	log.Printf("[DEBUG] Found user: %s", email)
+	log.Printf("[DEBUG] Found user for email: %s", email)
 
 	s.lock.RLock()
 	defer s.lock.RUnlock()
@@ -90,7 +88,7 @@ func (s *slackClient) PostMessage(options ...slack.MsgOption) error {
 	return err
 }
 
-func (s *slackClient) PostMessageToUser(user slack.User, options ...slack.MsgOption) error {
+func (s *slackClient) PostMessageToUser(user *slack.User, options ...slack.MsgOption) error {
 	_, _, err := s.client.PostMessage(user.ID, options...)
 	return err
 }
