0129b4a9fdc7fb240ee33f0e0f6f64f0bc33aad4
Tom Martensen <tmartens@redhat.com>
2023-04-05 13:33:53 +0200

    fixes

--- scripts/install-buf.sh
+++ scripts/install-buf.sh
@@ -9,8 +9,8 @@ INSTALLED_BUF_VERSION=$(buf --version || echo "not installed")
 
 if [ "$BUF_VERSION" != "${INSTALLED_BUF_VERSION}" ]; then
     mkdir -p "${GO_BIN_DIR}"
-    curl -SL \
+    curl -sSL \
         "https://github.com/bufbuild/buf/releases/download/v${BUF_VERSION}/buf-$(uname -s)-$(uname -m)" \
         -o "${GO_BIN_DIR}/buf" && \
-    chmod +x "{GO_BIN_DIR}/buf"
+    chmod +x "${GO_BIN_DIR}/buf"
 fi
