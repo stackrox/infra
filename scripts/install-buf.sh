f75387c5149225183d57152143629f62ddcaa8af
Tom Martensen <tmartens@redhat.com>
2023-04-05 13:27:48 +0200

    debug

--- scripts/install-buf.sh
+++ scripts/install-buf.sh
@@ -3,13 +3,16 @@
 set -xeuo pipefail
 
 go env GOPATH
+go env GOARCH
+go env GOOS
+
 uname -s
 uname -m
 
 BUF_VERSION="1.16.0"
 INSTALLED_BUF_VERSION=$(buf --version || echo "not installed")
 if [ "$BUF_VERSION" != "${INSTALLED_BUF_VERSION}" ]; then
-    curl -sSL \
+    curl -SL \
         "https://github.com/bufbuild/buf/releases/download/v${BUF_VERSION}/buf-$(uname -s)-$(uname -m)" \
         -o "$(go env GOPATH)/bin/buf" && \
     chmod +x "$(go env GOPATH)/bin/buf"
