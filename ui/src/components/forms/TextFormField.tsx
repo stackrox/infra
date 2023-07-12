e0545964334969b9e56e2c9bd6e60645a1088111
Gavin Jefferies <gavin@stackrox.com>
2023-07-12 13:47:09 -0700

    restore bottom margin

--- ui/src/components/forms/TextFormField.tsx
+++ ui/src/components/forms/TextFormField.tsx
@@ -29,44 +29,46 @@ export default function TextFormField({
   };
 
   return (
-    <FormGroup
-      label={label}
-      fieldId={id}
-      isRequired={required}
-      labelIcon={
-        helperText ? (
-          <Popover bodyContent={<div>{helperText}</div>}>
-            <button
-              type="button"
-              aria-label={`Help for ${name}`}
-              onClick={(e) => e.preventDefault()}
-              aria-describedby={id}
-              className="pf-c-form__group-label-help"
-            >
-              <HelpIcon noVerticalAlign />
-            </button>
-          </Popover>
-        ) : undefined
-      }
-      validated={meta.error ? ValidatedOptions.error : ValidatedOptions.default}
-      helperTextInvalid={meta.error}
-      className="capitalize"
-    >
-      <TextInput
-        id={id}
-        name={name}
-        onChange={onChange}
-        type="text"
-        value={field.value} // eslint-disable-line @typescript-eslint/no-unsafe-assignment
-        placeholder={placeholder}
+    <div className="mb-4">
+      <FormGroup
+        label={label}
+        fieldId={id}
         isRequired={required}
-        isDisabled={disabled}
-        aria-describedby={`${id}-helper`}
+        labelIcon={
+          helperText ? (
+            <Popover bodyContent={<div>{helperText}</div>}>
+              <button
+                type="button"
+                aria-label={`Help for ${name}`}
+                onClick={(e) => e.preventDefault()}
+                aria-describedby={id}
+                className="pf-c-form__group-label-help"
+              >
+                <HelpIcon noVerticalAlign />
+              </button>
+            </Popover>
+          ) : undefined
+        }
         validated={meta.error ? ValidatedOptions.error : ValidatedOptions.default}
-        className={`bg-base-100 border-2 rounded p-2 my-2 border-base-300 font-600 text-base-600 leading-normal min-h-8 ${
-          disabled ? 'bg-base-200' : 'hover:border-base-400'
-        }`}
-      />
-    </FormGroup>
+        helperTextInvalid={meta.error}
+        className="capitalize"
+      >
+        <TextInput
+          id={id}
+          name={name}
+          onChange={onChange}
+          type="text"
+          value={field.value} // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+          placeholder={placeholder}
+          isRequired={required}
+          isDisabled={disabled}
+          aria-describedby={`${id}-helper`}
+          validated={meta.error ? ValidatedOptions.error : ValidatedOptions.default}
+          className={`bg-base-100 border-2 rounded p-2 my-2 border-base-300 font-600 text-base-600 leading-normal min-h-8 ${
+            disabled ? 'bg-base-200' : 'hover:border-base-400'
+          }`}
+        />
+      </FormGroup>
+    </div>
   );
 }
