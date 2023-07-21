01b35d4712e75be29c7df5b18e21a452e13fd299
Gavin Jefferies <gavin@stackrox.com>
2023-07-21 15:59:27 -0700

    Switch header to PatternFly (#916)

--- ui/src/components/AppHeaderLayout.tsx
+++ ui/src/components/AppHeaderLayout.tsx
@@ -1,4 +1,11 @@
 import React, { ReactElement, ReactNode } from 'react';
+import {
+  Masthead,
+  MastheadMain,
+  MastheadBrand,
+  MastheadContent,
+  ToolbarItem,
+} from '@patternfly/react-core';
 
 type Props = {
   logo: ReactNode;
@@ -8,12 +15,15 @@ type Props = {
 
 export default function AppHeaderLayout({ logo, main, ending }: Props): ReactElement {
   return (
-    <header>
-      <nav className="top-navigation flex bg-base-100">
-        <div>{logo}</div>
-        <div>{main}</div>
-        <div className="ml-auto">{ending}</div>
-      </nav>
-    </header>
+    <Masthead>
+      <MastheadMain>
+        <MastheadBrand className="pf-u-mr-xl">{logo}</MastheadBrand>
+        <ToolbarItem variant="separator" />
+        {main}
+      </MastheadMain>
+      <MastheadContent className="pf-u-flex-direction-row-reverse">
+        <div>{ending}</div>
+      </MastheadContent>
+    </Masthead>
   );
 }
