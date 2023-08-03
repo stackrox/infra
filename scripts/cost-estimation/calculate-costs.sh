20a972f8335f0da0b18e85814fc9f0c6b66f0b95
Tom Martensen <tmartens@redhat.com>
2023-08-03 10:18:52 +0200

    debug

--- scripts/cost-estimation/calculate-costs.sh
+++ scripts/cost-estimation/calculate-costs.sh
@@ -4,11 +4,10 @@ set +xeuo pipefail
 
 WORKING_DIR=$(dirname "$0")
 
-ls -lisa "$WORKING_DIR"
-
 bq query \
     --nouse_legacy_sql \
     --project_id stackrox-infra \
     --format prettyjson \
 < "${WORKING_DIR}/total-time-consumed.sql" \
+| wc -l
 # | python3 "${WORKING_DIR}/render_costs.py"
