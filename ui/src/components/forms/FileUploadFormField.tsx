b35cfd0f6e3c0b8c3bab5b9eb18e82b10899a968
Gavin Jefferies <gavin@stackrox.com>
2023-08-04 18:06:28 -0700

    pattern fly the file upload

--- ui/src/components/forms/FileUploadFormField.tsx
+++ ui/src/components/forms/FileUploadFormField.tsx
@@ -1,17 +1,14 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import { useField } from 'formik';
-
-import FormFieldLabel from './FormFieldLabel';
-import FormFieldError from './FormFieldError';
+import { FormGroup, Popover, FileUpload, ValidatedOptions } from '@patternfly/react-core';
+import { HelpIcon } from '@patternfly/react-icons';
 
 type Props = {
   name: string;
   id?: string;
   required?: boolean;
   label: string;
-  placeholder?: string;
   helperText?: string;
-  disabled?: boolean;
 };
 
 export default function FileUploadFormField({
@@ -19,62 +16,77 @@ export default function FileUploadFormField({
   id = `file-input-${name}`,
   required = false,
   label,
-  placeholder = '',
   helperText = '',
-  disabled = false,
 }: Props): ReactElement {
   const [field, meta, helpers] = useField(name);
-  const [reader, setReader] = useState<FileReader | null>(null);
+  const [filename, setFilename] = React.useState('');
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const handleFileInputChange = (
+    event: React.ChangeEvent<HTMLInputElement> | React.DragEvent<HTMLElement>,
+    file: File
+  ) => {
+    setFilename(file.name);
+  };
+
+  const handleTextOrDataChange = (value: string) => {
+    helpers.setValue(value);
+  };
+
+  const handleClear = (_event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setFilename('');
+    helpers.setValue('');
+  };
+
+  const handleFileReadStarted = (_fileHandle: File) => {
+    setIsLoading(true);
+  };
+
+  const handleFileReadFinished = (_fileHandle: File) => {
+    setIsLoading(false);
+  };
 
   return (
-    <div className="flex flex-col mb-4">
-      <FormFieldLabel text={label} labelFor={id} required={required} />
-
-      {helperText.length > 0 && <span className="font-400 text-base-600 my-1">{helperText}</span>}
-
-      <input
-        id={`${id}-filename`}
-        type="file"
-        name={`${name}-filename`}
-        placeholder={placeholder}
-        disabled={disabled}
-        onChange={(e): void => {
-          if (reader) {
-            reader.abort();
-          }
-          if (e.target.files) {
-            const thisReader = new FileReader();
-            thisReader.addEventListener('loadstart', () => {
-              helpers.setError('');
-              helpers.setValue('');
-            });
-            thisReader.addEventListener('load', (loadEvent) => {
-              if (loadEvent.target?.result) {
-                helpers.setValue(loadEvent.target.result);
-              }
-            });
-            thisReader.addEventListener('error', () => {
-              helpers.setError('The file could not be read. Check permissions and try again.');
-            });
-            thisReader.readAsText(e.target.files[0]);
-            setReader(thisReader);
-          } else {
-            setReader(null);
-          }
-        }}
-        className={`bg-base-100 border-2 rounded p-2 my-2 border-base-300 font-600 text-base-600 leading-normal min-h-8 ${
-          disabled ? 'bg-base-200' : 'hover:border-base-400'
-        }`}
-      />
-
-      <input
-        {...field} // eslint-disable-line react/jsx-props-no-spreading
+    <FormGroup
+      label={label}
+      fieldId={id}
+      isRequired={required}
+      labelIcon={
+        helperText ? (
+          <Popover bodyContent={<div>{helperText}</div>}>
+            <button
+              type="button"
+              aria-label={`Help for ${name}`}
+              onClick={(e) => e.preventDefault()}
+              aria-describedby={id}
+              className="pf-c-form__group-label-help"
+            >
+              <HelpIcon noVerticalAlign />
+            </button>
+          </Popover>
+        ) : undefined
+      }
+      validated={meta.error ? ValidatedOptions.error : ValidatedOptions.default}
+      helperTextInvalid={meta.error}
+    >
+      <FileUpload
         id={id}
-        type="hidden"
-        name={name}
+        value={field.value} // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+        filename={filename}
+        filenamePlaceholder="Drag and drop a file or upload one"
+        type="text"
+        hideDefaultPreview
+        onFileInputChange={handleFileInputChange}
+        onDataChange={handleTextOrDataChange}
+        onTextChange={handleTextOrDataChange}
+        onReadStarted={handleFileReadStarted}
+        onReadFinished={handleFileReadFinished}
+        onClearClick={handleClear}
+        isLoading={isLoading}
+        browseButtonText="Upload"
+        aria-describedby={`${id}-helper`}
+        validated={meta.error ? ValidatedOptions.error : ValidatedOptions.default}
       />
-
-      <FormFieldError error={meta.error} touched />
-    </div>
+    </FormGroup>
   );
 }
