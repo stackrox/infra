659fecd29a420515f53bbafb7b255bb0ac8b52fe
Gavin Jefferies <gavin@stackrox.com>
2023-08-17 16:31:06 -0700

    Switch FullPageError to PatternFly classes

--- ui/src/components/FullPageError.tsx
+++ ui/src/components/FullPageError.tsx
@@ -11,9 +11,9 @@ export default function FullPageError({
   IconComponent = AlertCircle,
 }: Props): ReactElement {
   return (
-    <div className="flex flex-row w-full h-full items-center justify-center bg-base-0">
-      <IconComponent size={64} />
-      <span className="pl-2 text-4xl">{message}</span>
+    <div className="pf-u-display-flex pf-u-align-items-center pf-u-justify-content-center pf-u-h-100">
+      <IconComponent className="pf-u-mr-md" size={64} />
+      <span className="pf-u-font-size-4xl">{message}</span>
     </div>
   );
 }
