2c05d2dde7e2ce163e71fe2478f574d0811dfc7e
Gavin Jefferies <gavin@stackrox.com>
2023-08-21 08:02:23 -0700

    Remove ErrorBoundary (#957)

--- ui/src/containers/LaunchClusterPage/LaunchClusterPage.tsx
+++ ui/src/containers/LaunchClusterPage/LaunchClusterPage.tsx
@@ -5,7 +5,6 @@ import { FlavorServiceApi } from 'generated/client';
 import useApiQuery from 'client/useApiQuery';
 import configuration from 'client/configuration';
 import PageSection from 'components/PageSection';
-import ErrorBoundary from 'components/ErrorBoundary';
 import FullPageSpinner from 'components/FullPageSpinner';
 import FullPageError from 'components/FullPageError';
 import ClusterForm from './ClusterForm';
@@ -28,13 +27,11 @@ export default function LaunchClusterPage(): ReactElement {
 
   return (
     <PageSection header={`Launch "${data.Name}" Cluster (${data?.Availability || 'Alpha'})`}>
-      <ErrorBoundary message="UI doesn't support this flavor yet. Use infractl instead.">
-        <ClusterForm
-          flavorId={flavorId}
-          flavorParameters={data.Parameters}
-          onClusterCreated={(clusterId): void => navigate(`/cluster/${clusterId}`)}
-        />
-      </ErrorBoundary>
+      <ClusterForm
+        flavorId={flavorId}
+        flavorParameters={data.Parameters}
+        onClusterCreated={(clusterId): void => navigate(`/cluster/${clusterId}`)}
+      />
     </PageSection>
   );
 }
