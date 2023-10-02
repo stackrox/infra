a87ead70f585a689121e1e912cbf288040323df9
Linda Song <lindahsong@gmail.com>
2023-10-02 11:26:26 -0700

    Hackathon '23: Adding all clusters to url and drag/drop window split screen (#1013)

--- ui/src/containers/HomePage/HomePage.tsx
+++ ui/src/containers/HomePage/HomePage.tsx
@@ -1,13 +1,31 @@
 import React, { ReactElement } from 'react';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerContentBody,
+  DrawerPanelContent,
+  DrawerHead,
+} from '@patternfly/react-core';
 
 import LaunchPageSection from './LaunchPageSection';
 import MyClustersPageSection from './MyClustersPageSection';
 
 export default function HomePage(): ReactElement {
   return (
-    <>
-      <LaunchPageSection />
-      <MyClustersPageSection />
-    </>
+    <Drawer isExpanded isInline position="bottom">
+      <DrawerContent
+        panelContent={
+          <DrawerPanelContent isResizable defaultSize="50%" minSize="150px">
+            <DrawerHead>
+              <MyClustersPageSection />
+            </DrawerHead>
+          </DrawerPanelContent>
+        }
+      >
+        <DrawerContentBody>
+          <LaunchPageSection />
+        </DrawerContentBody>
+      </DrawerContent>
+    </Drawer>
   );
 }
