206b43d5709a86a295df87a995dc0288037d40b8
Gavin Jefferies <gjefferi@redhat.com>
2023-10-13 14:39:53 -0700

    PatternFly the PageSection component (#1036)

--- ui/src/components/PageSection.tsx
+++ ui/src/components/PageSection.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement, ReactNode } from 'react';
+import { Panel, PanelMain, PanelMainBody, PanelHeader, Divider } from '@patternfly/react-core';
 
 type Props = {
   header: ReactNode;
@@ -8,15 +9,12 @@ type Props = {
 
 export default function PageSection({ header, className = '', children }: Props): ReactElement {
   return (
-    <div className="border-t-3 border-base-400 flex flex-col h-full min-h-0">
-      <div className={`h-full overflow-auto ${className}`}>
-        <header className="border-b-2 border-base-400 px-4 py-2">
-          <h2 className="bg-base-0 capitalize font-600 sticky text-4xl text-base-600 top-0">
-            {header}
-          </h2>
-        </header>
-        <div className="flex flex-col p-4">{children}</div>
-      </div>
-    </div>
+    <Panel className={className}>
+      <PanelHeader className="pf-u-font-size-2xl">{header}</PanelHeader>
+      <Divider />
+      <PanelMain>
+        <PanelMainBody>{children}</PanelMainBody>
+      </PanelMain>
+    </Panel>
   );
 }
