67d3417c1c239fa123244c6a2907ce0f2b69f131
Misha Sugakov <msugakov@redhat.com>
2026-03-05 13:57:56 +0100

    Introduce reusable functions for adding and reading a flag

--- cmd/infractl/common/wait.go
+++ cmd/infractl/common/wait.go
@@ -6,12 +6,28 @@ import (
 	"os"
 	"time"
 
+	"github.com/spf13/cobra"
 	v1 "github.com/stackrox/infra/generated/api/v1"
 )
 
-const DefaultMaxConsecutiveWaitErrors = 10
+const (
+	flagName                        = "wait-max-errors"
+	defaultMaxConsecutiveWaitErrors = 10
+)
 
-func WaitForCluster(client v1.ClusterServiceClient, clusterID *v1.ResourceByID, maxConsecutiveErrors int) error {
+func AddMaxWaitErrorsFlag(cmd *cobra.Command) {
+	cmd.Flags().Int(flagName, defaultMaxConsecutiveWaitErrors, "maximum number of consecutive errors before giving up waiting")
+}
+
+func GetMaxWaitErrorsFlagValue(cmd *cobra.Command) int {
+	maxWaitErrors, err := cmd.Flags().GetInt(flagName)
+	if err != nil {
+		panic(err)
+	}
+	return maxWaitErrors
+}
+
+func WaitForCluster(client v1.ClusterServiceClient, clusterID *v1.ResourceByID, maxWaitErrors int) error {
 	const timeoutSleep = 30 * time.Second
 
 	nErrors := 0
@@ -25,7 +41,7 @@ func WaitForCluster(client v1.ClusterServiceClient, clusterID *v1.ResourceByID, 
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "...error %s\n", err)
 			nErrors += 1
-			if nErrors >= maxConsecutiveErrors {
+			if nErrors >= maxWaitErrors {
 				return errors.New("too many errors while waiting")
 			}
 		} else {
