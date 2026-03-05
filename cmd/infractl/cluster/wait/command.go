8f1637ef9263521911150904a823aa8198a4de82
Misha Sugakov <msugakov@redhat.com>
2026-03-05 10:12:36 +0100

    Set up wait command as a copy of get

---
+++ cmd/infractl/cluster/wait/command.go
@@ -0,0 +1,45 @@
+// Package get implements the infractl get command.
+package get
+
+import (
+	"context"
+	"errors"
+
+	"github.com/spf13/cobra"
+	"github.com/stackrox/infra/cmd/infractl/cluster/utils"
+	"github.com/stackrox/infra/cmd/infractl/common"
+	v1 "github.com/stackrox/infra/generated/api/v1"
+	"google.golang.org/grpc"
+)
+
+const examples = `Lookup info for the "example-s3maj" cluster.
+$ infractl get example-s3maj`
+
+// Command defines the handler for infractl get.
+func Command() *cobra.Command {
+	// $ infractl get
+	return &cobra.Command{
+		Use:     "get CLUSTER",
+		Short:   "Get info for a specific cluster",
+		Long:    "Displays info for a single cluster",
+		Example: examples,
+		Args:    common.ArgsWithHelp(cobra.ExactArgs(1), args),
+		RunE:    common.WithGRPCHandler(run),
+	}
+}
+
+func args(_ *cobra.Command, args []string) error {
+	if args[0] == "" {
+		return errors.New("no cluster ID given")
+	}
+	return utils.ValidateClusterName(args[0])
+}
+
+func run(ctx context.Context, conn *grpc.ClientConn, _ *cobra.Command, args []string) (common.PrettyPrinter, error) {
+	resp, err := v1.NewClusterServiceClient(conn).Info(ctx, &v1.ResourceByID{Id: args[0]})
+	if err != nil {
+		return nil, err
+	}
+
+	return &prettyCluster{resp}, nil
+}
